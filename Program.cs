6e899af01911e4d88e3d5b6645f3be2e4581090a
Konstantino123 <kt79@bk.ru>
2022-10-23 18:44:22 +0300

    new file

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+{
+Console.WriteLine("Введите трехзначное число: ");
+int num = Convert.ToInt16(Console.ReadLine());
+int A = num % 100;
+int B = A / 10; 
+if ((num > 99 && num <= 999))
+    
+    Console.WriteLine(B);
+     
+    
+
+else
+{
+    Console.WriteLine("Вы ввели не трехначное число");
+}    
+}
+ 
+
+
+
+ 
+
+
+    
+
